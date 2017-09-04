58e56bc61d507293a16a076ea33042b0527df99f
jplourd <jeffplourd@gmail.com>
2017-09-04 08:45:14 -0500

    adds signup and signin views

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignupComponent } from './containers/signup/signup.component';
+import { SigninComponent } from './containers/signin/signin.component';
 
 const routes: Routes = [
   {
     path: '',
-    children: []
+    redirectTo: '/signin',
+    pathMatch: 'full'
+  },
+  {
+    path: 'signin',
+    component: SigninComponent
+  },
+  {
+    path: 'signup',
+    component: SignupComponent
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
